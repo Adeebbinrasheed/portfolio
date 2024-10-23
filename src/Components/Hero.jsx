e723e6dc13e9125e6928bd26af899a435c375b25
adeeb <adeebcheerangan1111@gmail.com>
2024-10-23 14:47:17 +0530

    commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -85,6 +85,11 @@ const Hero = () => {
             </button>
           </a>
         </div>
+        <button class="before:ease relative h-12 w-40 overflow-hidden border rounded-xl border-black text-black shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
+          <span class="relative z-10">
+            <a href="/">Resume</a>
+          </span>
+        </button>
       </motion.div>
 
       <FloatingWhatsApp
